f3adf46293ce04424f3acd6cdc9b6068f369184e
2200090246 <2200090246@kluniversity.in>
2026-09-02 11:00:33 +0530

    developed the bank account number automatic genrator while adding cutomer or user

--- sampleproject/src/com/tasks/Account.java
+++ sampleproject/src/com/tasks/Account.java
@@ -16,20 +16,20 @@ public class Account {
 		a.accountHolderName = "Mahesh";
 		a.balance = 400000;
 		System.out.println(a.accountHolderName + " Details");
-		System.out.println("Account Holder Name : " + a.accountHolderName + " Account Number : " + a.Acno
-				+ " Balance : " + a.balance);
+		System.out.println("Account Holder Name : " + a.accountHolderName + ", Account Number : " + a.Acno
+				+ ", Balance : " + a.balance);
 		Account b = new Account();
 		b.accountHolderName = "Subhu";
 		b.balance = 500000;
 		System.out.println(b.accountHolderName + " Details");
-		System.out.println("Account Holder Name : " + b.accountHolderName + " Account Number : " + b.Acno
-				+ " Balance : " + b.balance);
+		System.out.println("Account Holder Name : " + b.accountHolderName + ", Account Number : " + b.Acno
+				+ ", Balance : " + b.balance);
 		Account c = new Account();
 		c.accountHolderName = "Mokshitha";
 		c.balance = 600000;
 		System.out.println(c.accountHolderName + " Details");
-		System.out.println("Account Holder Name : " + c.accountHolderName + " Account Number : " + c.Acno
-				+ " Balance : " + c.balance);
+		System.out.println("Account Holder Name : " + c.accountHolderName + ", Account Number : " + c.Acno
+				+ ", Balance : " + c.balance);
 
 	}
 
