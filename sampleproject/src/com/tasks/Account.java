9beb3a62f1e4297c98b9d2b21208ebbe14e3cedf
2200090246 <2200090246@kluniversity.in>
2026-09-02 10:59:54 +0530

    devloped automatic account number genrator while adding cutomers or users

---
+++ sampleproject/src/com/tasks/Account.java
@@ -0,0 +1,36 @@
+package com.tasks;
+
+public class Account {
+	int Acno;
+	String accountHolderName;
+	double balance;
+	static int accnogenrator = 2463;
+	{
+		Acno = accnogenrator;
+		accnogenrator++;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account a = new Account();
+		a.accountHolderName = "Mahesh";
+		a.balance = 400000;
+		System.out.println(a.accountHolderName + " Details");
+		System.out.println("Account Holder Name : " + a.accountHolderName + " Account Number : " + a.Acno
+				+ " Balance : " + a.balance);
+		Account b = new Account();
+		b.accountHolderName = "Subhu";
+		b.balance = 500000;
+		System.out.println(b.accountHolderName + " Details");
+		System.out.println("Account Holder Name : " + b.accountHolderName + " Account Number : " + b.Acno
+				+ " Balance : " + b.balance);
+		Account c = new Account();
+		c.accountHolderName = "Mokshitha";
+		c.balance = 600000;
+		System.out.println(c.accountHolderName + " Details");
+		System.out.println("Account Holder Name : " + c.accountHolderName + " Account Number : " + c.Acno
+				+ " Balance : " + c.balance);
+
+	}
+
+}
