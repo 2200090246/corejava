abc9b48c5149cfda104f194bf12f8d8bf0deffdf
2200090246 <2200090246@kluniversity.in>
2026-09-05 22:41:13 +0530

    I have tested that how a program executes or which part in a program executes first

--- sampleproject/src/com/mahesh/Link.java
+++ sampleproject/src/com/mahesh/Link.java
@@ -1,3 +1,4 @@
+//Task: check in a program which part will be executed first
 package com.mahesh;
 
 public class Link {
@@ -6,6 +7,7 @@ public class Link {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome mahesh");
 	}
+	//static block will get execute first because when class loads it will first check for static block or variables after executing them it will execute the main method
 	static {
 		System.out.println("Welcome to jvm architecture");
 	}
