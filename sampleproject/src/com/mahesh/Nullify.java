923d62000e878a4e65726bc252397a43fe4e3a29
2200090246 <2200090246@kluniversity.in>
2026-09-05 22:52:00 +0530

    i have written this program to know how garbage collector works and how the objects are eligible for garbage collector

--- sampleproject/src/com/mahesh/Nullify.java
+++ sampleproject/src/com/mahesh/Nullify.java
@@ -1,3 +1,4 @@
+//Task: write a program and check how garbage collector works
 package com.mahesh;
 
 public class Nullify {
@@ -5,14 +6,15 @@ public class Nullify {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Nullify n = new Nullify();
-	    Nullify n2 = new Nullify();
-	    n = n2;
-	    n2= null;
-	    n = null;
-		System.gc();
+		Nullify n = new Nullify();//when i create object named n it will referenced to one object like n -> object1
+	    Nullify n2 = new Nullify();//when i create object named n2 it will referenced another object like n2 -> onject2
+	    n = n2;//here we are saying that now hey n from now you don't have to refer object1 you have to refer object 2 like n -> object 2 and n2 -> object 2
+	    n2= null;//here when we null the n2 then it will be unreachable 
+	    n = null;//here when we null the n then it will be unreachable
+		System.gc();// when we call this system.gc() it will automatically collect the unreferenced objects 
 
 	}
+	//to confirm that garabage collector called we will use finalize method actually JVM automatically calls garbage collector and at that time it will call finalize method as we want to print something that we want so we are overriding it from object class
 	 public void finalize() {
         System.out.println("Object is garbage collected");
     }
