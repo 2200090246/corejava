b9d352f431e9452428024ae5ce34bfe638d036fe
2200090246 <2200090246@kluniversity.in>
2026-09-05 22:56:38 +0530

    I Checked how JVM actualy loads the class

--- sampleproject/src/com/mahesh/Student.java
+++ sampleproject/src/com/mahesh/Student.java
@@ -1,3 +1,4 @@
+//Task: write a program to understand how JVM finds a class
 package com.mahesh;
 
 public class Student {
@@ -6,9 +7,9 @@ public class Student {
 		// TODO Auto-generated method stub
 		System.out.println("Mainmethod started");
 		System.out.println("welcome to java world");
-		Class.forName("java.lang.System");
+		Class.forName("java.lang.System");//As it is predefined package JVM will find this class at bootstrap class loader
 		Class.forName("java.lang.Class");
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");//As it is external package it will load in application class loader
 		Class.forName("com.mahesh.Test");
 		System.out.println("Mainmethod ended");
 	}
