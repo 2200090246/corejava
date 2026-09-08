60d6592904afcb68d35b1cfaa0f05f2cd517697e
2200090246 <2200090246@kluniversity.in>
2026-09-08 10:24:34 +0530

    written a program to count number of objects created using instance block

---
+++ sampleproject/src/com/tasks/CountOfObjects.java
@@ -0,0 +1,29 @@
+//Task: Write a program to count the number of objects created using loops and check how many objects are eligible for garbage collector;
+
+package com.tasks;
+
+public class CountOfObjects {
+static int count = 0; // As we want to increase count when new object created we are using static variable
+	
+	static{
+		System.out.println("Counting number of objects created");
+	}
+	{
+		count++;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+			CountOfObjects c = new CountOfObjects();
+		}
+		System.out.println("No of objects created : "+count);
+		System.gc();
+
+	}
+	@Override
+	protected void finalize() {
+		// TODO Auto-generated method stub
+		System.out.println("object is eligible for gc");
+	}
+
+}
