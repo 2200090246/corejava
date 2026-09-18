388fa1c4e49ccdf40e6e156e3f9aa21b1de4c597
2200090246 <2200090246@kluniversity.in>
2026-09-18 21:36:38 +0530

    Check weather a number is even or odd

---
+++ sampleproject/src/com/methods/nrwp/EvenOdd.java
@@ -0,0 +1,20 @@
+package com.methods.nrwp;
+import java.util.Scanner;
+public class EvenOdd {
+	public void check(int number) {
+		if(number%2==0) System.out.println("Even");
+		else System.out.println("Odd");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int number = sc.nextInt();
+		EvenOdd e = new EvenOdd();
+		e.check(number);
+		sc.close();
+		
+
+	}
+
+}
