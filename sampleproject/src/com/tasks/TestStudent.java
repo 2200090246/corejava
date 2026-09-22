3ee6d479ec8ea85a44a82baf62c06f7a2d2462f7
2200090246 <2200090246@kluniversity.in>
2026-09-22 11:37:55 +0530

    Understanding how a constructor works

---
+++ sampleproject/src/com/tasks/TestStudent.java
@@ -0,0 +1,13 @@
+package com.tasks;
+
+public class TestStudent {
+	TestStudent(){
+		System.out.println("No-arg Constructor called");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestStudent t = new TestStudent();
+
+	}
+
+}
